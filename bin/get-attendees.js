b7a53feeb759bf1448088dab302149b472dcfef5
Tim Severien <timseverien@gmail.com>
2017-06-13 13:50:16 +0200

    Add script to fetch list of attendees

---
+++ bin/get-attendees.js
@@ -0,0 +1,43 @@
+const request = require('request');
+
+const EDITION_START = 2008;
+const EDITION_END = (new Date()).getFullYear();
+
+const getEditions = (start, end) => {
+    const editions = [];
+
+    for (let year = EDITION_START; year <= EDITION_END; year++) {
+        editions.push(year);
+    }
+
+    return editions;
+};
+
+const getAttendees = (year) => new Promise((resolve, reject) => {
+    const requestOptions = {
+        url: `https://fronteers.nl/congres/${year}/attendees.json`,
+        json: true,
+    };
+
+    request(requestOptions, (err, response, body) => {
+        if (err) return reject(err);
+
+        resolve(body[`fronteers${year}`]);
+    });
+});
+
+const normalize = (edition) => ({
+    amount: edition.numberOfAttendeesThisyear,
+    attendees: edition.andAllTheirOtherData.map((attendee) => ({
+        name: attendee.name,
+        company: attendee.company,
+        twitter: attendee.twitter,
+        isMember: !!attendee.awesome,
+    })),
+});
+
+Promise.all(getEditions().map(getAttendees))
+    .then(data => data.map(normalize))
+    .then(data => JSON.stringify(data))
+    .then(console.log)
+    .catch(console.error);
