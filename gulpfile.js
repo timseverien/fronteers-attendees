1af9ba6c0d89ea83dadb98aea5d5266de8d47018
Tim Severien <timseverien@gmail.com>
2017-06-14 15:03:05 +0200

    Improve workflow

--- gulpfile.js
+++ gulpfile.js
@@ -8,9 +8,12 @@ const source = require('vinyl-source-stream');
 
 const config = {
   scripts: {
-    entry: 'src/scripts/main.js',
     source: 'src/scripts/**/*.js',
     destination: 'docs/assets/scripts',
+  },
+  styles: {
+    source: 'src/styles/**/*.css',
+    destination: 'docs/assets/styles',
   },
   views: {
     entry: 'src/views/index.hbs',
@@ -20,14 +23,14 @@ const config = {
 };
 
 gulp.task('scripts', () => {
-  const options = {
-    entry: config.scripts.entry,
-    format: 'iife',
-  };
-
-  return rollup(options)
+  return rollup('rollup.config.js')
     .pipe(source('main.js'))
     .pipe(gulp.dest(config.scripts.destination));
+});
+
+gulp.task('styles', () => {
+  return gulp.src(config.styles.source)
+    .pipe(gulp.dest(config.styles.destination));
 });
 
 gulp.task('views', () => {
@@ -47,9 +50,10 @@ gulp.task('views', () => {
     .pipe(gulp.dest(config.views.destination));
 });
 
-gulp.task('default', done => runSequence('scripts', 'views', done));
+gulp.task('default', done => runSequence(['scripts', 'styles', 'views'], done));
 
 gulp.task('watch', ['default'], () => {
   gulp.watch(config.scripts.source, ['scripts']);
+  gulp.watch(config.styles.source, ['styles']);
   gulp.watch(config.views.source, ['views']);
 });
